c3ff6dbaf92452b9a324eba66f2f30d32366658d
Dennis P <43825430+silverDennis@users.noreply.github.com>
2019-05-22 14:11:24 -0700

    Add files via upload

---
+++ src/Cloud.java
@@ -0,0 +1,29 @@
+
+/**
+ * 
+ * @author Benjamen Bielecki, Dennis Perepech
+ * @version 1.0
+ * 
+ */
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import javafx.scene.image.Image;
+
+public class Cloud extends Obstacle {
+	public Cloud(int x, int y, int w, int h, int vx, int vy) {
+		super(setImage(), x, y, w, h, vx, vy);
+
+	}
+
+	private static Image setImage() {
+		Image img = null;
+		try {
+			img = new Image(new FileInputStream("media/cloud.png"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return img;
+	}
+}
