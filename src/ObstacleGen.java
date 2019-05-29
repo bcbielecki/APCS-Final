7bb51f7ba2dfc0c80aeecb5745e1657fc73ccb74
Dennis <Dennis@PLUTO.lab>
2019-05-29 15:38:20 -0700

    Adjusted code structure

--- src/ObstacleGen.java
+++ src/ObstacleGen.java
@@ -13,6 +13,8 @@ import javafx.scene.paint.Color;
 public class ObstacleGen {
 	private GraphicsContext gc;
 	private List<Obstacle> obstacles;
+	private int counter;
+	private int initialSpawn = 6;
 	private final static int STAGE_1 = 60;
 	private final static int STAGE_2 = 120;
 	private final static int STAGE_3 = 180;
@@ -20,11 +22,13 @@ public class ObstacleGen {
 	
 	public ObstacleGen(GraphicsContext gc) {
 		this.gc = gc;
-		obstacles = new ArrayList<Obstacle>();
+		counter = 0;
 		
-		obstacles.add(new Cloud(Math.random() * gc.getCanvas().getWidth(), Math.random() * 20 - 20, 200.0, 100.0, 0, 5.0));
-		obstacles.add(new Cloud(Math.random() * gc.getCanvas().getWidth(), Math.random() * 20 - 20, 200.0, 100.0, 0, 5.0));
-		obstacles.add(new Cloud(Math.random() * gc.getCanvas().getWidth(), Math.random() * 20 - 20, 200.0, 100.0, 0, 5.0));
+		obstacles = new ArrayList<Obstacle>();
+		for(int i = 0; i < initialSpawn; i++) {
+			spawn();
+		}
+		
 	}
 	
 	public void generate(int counter) {
@@ -32,17 +36,34 @@ public class ObstacleGen {
 	
 	public void moveAll() {
 		for(int i = 0; i < obstacles.size(); i++) {
-			while(checkCollisionAllObstacles(obstacles.get(i), i)) {
-				obstacles.get(i).setX(Math.random() * (gc.getCanvas().getWidth() - (Math.random() * obstacles.get(i).getWidth())));
-				obstacles.get(i).setY((0 - (gc.getCanvas().getHeight() * 1.25) - (2 * obstacles.get(i).getHeight())) * Math.random());
-			}
-			
 			obstacles.get(i).move(gc);
-			if(obstacles.get(i).getY() > gc.getCanvas().getHeight() + 50) {
-				obstacles.get(i).setX(Math.random() * (gc.getCanvas().getWidth() - (Math.random() * obstacles.get(i).getWidth())));
-				obstacles.get(i).setY((0 - (gc.getCanvas().getHeight() * 1.25) - (2 * obstacles.get(i).getHeight())) * Math.random());
+			if(obstacles.get(i).getY() > gc.getCanvas().getHeight() + 50) { //50 as buffer to ensure off screen
+				obstacles.remove(i);
+				spawn();
 			}
 		}
+	}
+	
+	public void spawn() {
+		Obstacle obs = null;
+		if(counter >= 0 && counter < STAGE_1) {
+			obs = new Cloud(Math.random() * gc.getCanvas().getWidth() - 100, Math.random() * -1 * ((gc.getCanvas().getHeight() * 1.25)) - 100, 200.0, 100.0, 0, 5.0);
+		}
+		else if(counter > STAGE_1 && counter < STAGE_2) {
+			
+		}
+		else if(counter > STAGE_2 && counter < STAGE_3) {
+			
+		}
+		else if(counter > STAGE_3 && counter < STAGE_4) {
+			
+		}
+		else if(counter > STAGE_4) {
+			
+		}
+		
+		obstacles.add(obs);
+		adjustObstacles();
 	}
 	
 	public boolean checkCollisionAll(Sprite thing) {
@@ -52,6 +73,20 @@ public class ObstacleGen {
 			}
 		}
 		return false;
+	}
+	
+	public void adjustObstacles() {
+		for(int i = 0; i < obstacles.size(); i++) {
+			if(obstacles.get(i).getY() > 0) {
+				continue;
+			}
+			else {
+				while(checkCollisionAllObstacles(obstacles.get(i), i)) {
+					obstacles.get(i).setX(Math.random() * (gc.getCanvas().getWidth() - obstacles.get(i).getWidth()));
+					obstacles.get(i).setY(Math.random() * -1 * ((gc.getCanvas().getHeight() * 1.25)) - obstacles.get(i).getHeight());
+				}
+			}
+		}
 	}
 	
 	public boolean checkCollisionAllObstacles(Sprite thing, int index) {
@@ -73,5 +108,13 @@ public class ObstacleGen {
 			gc.setFill(Color.WHITE);
 			gc.strokeRect(obs.getX(), obs.getY(), obs.getWidth(), obs.getHeight());
 		}
+	}
+	
+	public void counterAdd() {
+		counter++;
+	}
+	
+	public int getCounter() {
+		return counter;
 	}
 }
